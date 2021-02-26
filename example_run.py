b1b6f42d11b4f3f2b0b9856418ad710183706a02
Max Shen <maxwshen@gmail.com>
2021-02-26 16:01:38 +0000

    update datasets

--- example_run.py
+++ example_run.py
@@ -2,24 +2,22 @@
   Example use of Evoracle
 '''
 
-
-# gene = 'abe8e'
 gene = 'cry1ac'
-
+rl = 100
 
 # Setup 
 inp_dir = 'example_data/'
-out_dir = f'example_evoracle_out_{gene}/'
+out_dir = f'example_evoracle_output/{gene}_{rl}nt/'
 
 # Load data
 import pandas as pd
 obs_reads_df = pd.read_csv(
-  inp_dir + f'{gene}_illumina_100nt_obsreads.csv'
+  inp_dir + f'{gene}_{rl}nt_obsreads.csv'
 )
 
 # Run Evoracle
 import evoracle
 evoracle.propose_gts_and_infer_fitness_and_frequencies(
   obs_reads_df = obs_reads_df, 
-  proposed_gt_out_fn = inp_dir + f'{gene}_proposedgts.txt', 
+  proposed_gt_out_fn = inp_dir + f'{gene}_{rl}nt_proposedgts.txt', 
   inference_out_dir = out_dir)
